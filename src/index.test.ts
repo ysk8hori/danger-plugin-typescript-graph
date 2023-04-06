fc75c5059746889bf185ee3dca69cd4e46dc27a9
ysk8hori <ysk8.hori@gmail.com>
2023-04-06 22:48:54 +0900

    refactor: prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,31 +1,30 @@
-import typescriptGraph from "./index"
+import typescriptGraph from './index';
 
-declare const global: any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const global: any;
 
-describe("typescriptGraph()", () => {
+describe('typescriptGraph()', () => {
   beforeEach(() => {
-    global.warn = jest.fn()
-    global.message = jest.fn()
-    global.fail = jest.fn()
-    global.markdown = jest.fn()
-  })
+    global.warn = jest.fn();
+    global.message = jest.fn();
+    global.fail = jest.fn();
+    global.markdown = jest.fn();
+  });
 
   afterEach(() => {
-    global.warn = undefined
-    global.message = undefined
-    global.fail = undefined
-    global.markdown = undefined
-  })
+    global.warn = undefined;
+    global.message = undefined;
+    global.fail = undefined;
+    global.markdown = undefined;
+  });
 
-  it("Checks for a that message has been called", () => {
+  it('Checks for a that message has been called', () => {
     global.danger = {
-      github: { pr: { title: "My Test Title" } },
-    }
+      github: { pr: { title: 'My Test Title' } },
+    };
 
-    typescriptGraph()
+    typescriptGraph();
 
-    expect(global.message).toHaveBeenCalledWith(
-      "PR Title: My Test Title",
-    )
-  })
-})
+    expect(global.message).toHaveBeenCalledWith('PR Title: My Test Title');
+  });
+});
