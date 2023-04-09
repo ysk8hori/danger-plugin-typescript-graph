fb9119b2de4714524484a567b08d95608235539a
ysk8hori <ysk8.hori@gmail.com>
2023-04-09 09:16:18 +0900

    feat: ログを仕込む

--- src/getFullGraph.ts
+++ src/getFullGraph.ts
@@ -4,6 +4,7 @@ import { Graph, Meta } from '@ysk8hori/typescript-graph/dist/src/models';
 import { execSync } from 'child_process';
 import { DangerDSLType } from 'danger/distribution/dsl/DangerDSL';
 import path = require('path');
+import { log } from './log';
 declare let danger: DangerDSLType;
 
 /**
@@ -21,17 +22,22 @@ export default function getFullGraph() {
   }>(resolve => {
     // head の Graph を生成
     const { graph: fullHeadGraph, meta } = createGraph(path.resolve('./'));
+    log('fullHeadGraph.nodes.length:', fullHeadGraph.nodes.length);
+    log('fullHeadGraph.relations.length:', fullHeadGraph.relations.length);
     // head には deleted 対象はない
     const headGraph = filterGraph(
       [danger.git.modified_files, danger.git.created_files].flat(),
       ['node_modules'],
       fullHeadGraph,
     );
+    log('headGraph:', headGraph);
 
     execSync(`git fetch origin ${danger.github.pr.base.ref}`);
     execSync(`git checkout ${danger.github.pr.base.ref}`);
     // base の Graph を生成
     const { graph: fullBaseGraph } = createGraph(path.resolve('./'));
+    log('fullBaseGraph.nodes.length:', fullBaseGraph.nodes.length);
+    log('fullBaseGraph.relations.length:', fullBaseGraph.relations.length);
     const baseGraph = filterGraph(
       [
         danger.git.modified_files,
@@ -41,6 +47,7 @@ export default function getFullGraph() {
       ['node_modules'],
       fullBaseGraph,
     );
+    log('baseGraph:', baseGraph);
     resolve({ headGraph, baseGraph, meta });
   });
 }
