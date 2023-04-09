dda892f760724905f8380f9d19dc48298f9a4826
ysk8hori <ysk8.hori@gmail.com>
2023-04-09 17:13:44 +0900

    feat(log): log 関数に指定されたメッセージがオブジェクトの場合は JSON.stringify する

--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function log(message?: any, ...optionalParams: any[]): void {
   if (!process.env.DEBUG) return;
-  console.log(message, ...optionalParams);
+  console.log(stringifyObject(message), ...optionalParams.map(stringifyObject));
+}
+
+function stringifyObject(message: any) {
+  // message がオブジェクトの場合は JSON.stringify で文字列化する
+  if (message !== null && typeof message === 'object') {
+    return JSON.stringify(message, null, 2);
+  }
+  return message;
 }
