b6cb70d9b178c3cc073c9a39eef0a2b221ee2194
ysk8hori <ysk8.hori@gmail.com>
2023-04-09 20:17:17 +0900

    fix(extractabstraction): 抽象化したくないディレクトリが抽象化されていた問題に対処

---
+++ src/extractAbstractionTarget.test.ts
@@ -0,0 +1,168 @@
+// extractAbstractionTarget のテスト
+import extractAbstractionTarget from './extractAbstractionTarget';
+
+it('グラフと、抽象化してはいけないファイルのパスから、抽象化して良いディレクトリのパスを取得する', () => {
+  expect(
+    extractAbstractionTarget(
+      {
+        nodes: [
+          {
+            path: 'src/components/game/cell/Hoge.ts',
+            name: 'Hoge.ts',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/components/game/cell/Cell.tsx',
+            name: 'Cell.tsx',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/components/game/utils/answers/getAnswerClass.ts',
+            name: 'getAnswerClass.ts',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/components/game/cell/MemoLayer.tsx',
+            name: 'MemoLayer.tsx',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/atoms.ts',
+            name: 'atoms.ts',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/components/game/GameBoard.tsx',
+            name: 'GameBoard.tsx',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/components/game/cell/Cell.stories.tsx',
+            name: 'Cell.stories.tsx',
+            changeStatus: 'not_modified',
+          },
+          {
+            path: 'src/components/game/cell/Cell.test.tsx',
+            name: 'Cell.test.tsx',
+            changeStatus: 'not_modified',
+          },
+        ],
+        relations: [
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/components/game/utils/answers/getAnswerClass.ts',
+              name: 'getAnswerClass.ts',
+              changeStatus: 'not_modified',
+            },
+            fullText: 'getAnswerClass',
+            changeStatus: 'not_modified',
+          },
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/components/game/cell/MemoLayer.tsx',
+              name: 'MemoLayer.tsx',
+              changeStatus: 'not_modified',
+            },
+            fullText: 'MemoLayer, { Props as MemoLayerProps }',
+            changeStatus: 'not_modified',
+          },
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/atoms.ts',
+              name: 'atoms.ts',
+              changeStatus: 'not_modified',
+            },
+            fullText: '{ AnswerImageVariant }',
+            changeStatus: 'not_modified',
+          },
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/components/game/cell/Hoge.ts',
+              name: 'Hoge.ts',
+              changeStatus: 'not_modified',
+            },
+            fullText: 'hoge',
+            changeStatus: 'not_modified',
+          },
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/GameBoard.tsx',
+              name: 'GameBoard.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            fullText: 'Cell',
+            changeStatus: 'not_modified',
+          },
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/cell/Cell.stories.tsx',
+              name: 'Cell.stories.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            fullText: 'Cell',
+            changeStatus: 'not_modified',
+          },
+          {
+            kind: 'depends_on',
+            from: {
+              path: 'src/components/game/cell/Cell.test.tsx',
+              name: 'Cell.test.tsx',
+              changeStatus: 'not_modified',
+            },
+            to: {
+              path: 'src/components/game/cell/Cell.tsx',
+              name: 'Cell.tsx',
+              changeStatus: 'not_modified',
+            },
+            fullText: 'Cell',
+            changeStatus: 'not_modified',
+          },
+        ],
+      },
+      [
+        '.github',
+        '.github/workflows',
+        'src',
+        'src/components',
+        'src/components/game',
+        'src/components/game/cell',
+      ],
+    ),
+  ).toEqual(['src/components/game/utils/answers']);
+});
