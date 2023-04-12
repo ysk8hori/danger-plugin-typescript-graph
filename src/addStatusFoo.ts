2c3fafc5be42d7be1b9dd4b5eeb7fa9eeadb19a1
ysk8hori <ysk8.hori@gmail.com>
2023-04-12 22:50:46 +0900

    feat(hoge): 30以上の変更を加えてグラフ表示が抑止されることを確認する

--- src/addStatusFoo.ts
+++ src/addStatusFoo.ts
@@ -2,6 +2,35 @@ import {
   Graph,
   ChangeStatus,
 } from '@ysk8hori/typescript-graph/dist/src/models';
+import hoge1 from './hoge1';
+import hoge2 from './hoge2';
+import hoge3 from './hoge3';
+import hoge4 from './hoge4';
+import hoge5 from './hoge5';
+import hoge6 from './hoge6';
+import hoge7 from './hoge7';
+import hoge8 from './hoge8';
+import hoge9 from './hoge9';
+import hoge10 from './hoge10';
+import hoge11 from './hoge11';
+import hoge12 from './hoge12';
+import hoge13 from './hoge13';
+import hoge14 from './hoge14';
+import hoge15 from './hoge15';
+import hoge16 from './hoge16';
+import hoge17 from './hoge17';
+import hoge18 from './hoge18';
+import hoge19 from './hoge19';
+import hoge20 from './hoge20';
+import hoge21 from './hoge21';
+import hoge22 from './hoge22';
+import hoge23 from './hoge23';
+import hoge24 from './hoge24';
+import hoge25 from './hoge25';
+import hoge26 from './hoge26';
+import hoge27 from './hoge27';
+import hoge28 from './hoge28';
+import hoge29 from './hoge29';
 
 export default function addStatus(
   {
