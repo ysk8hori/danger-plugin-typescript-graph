7a520c0031bc74a8cb72e1e4344b4f24e59babb6
ysk8hori <ysk8.hori@gmail.com>
2023-04-13 08:50:33 +0900

    refactor(config): 設定取得関数をまとめる config.ts を作成

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 /** 変更ファイル数が多い場合にグラフの表示を抑止するが、その際のノード数を指定する値を取得する。 */
-export default function getMaxSize() {
+export function getMaxSize() {
   // 環境変数の TSG_MAX_SIZE を返却する
   return process.env.TSG_MAX_SIZE ? parseInt(process.env.TSG_MAX_SIZE, 10) : 30;
 }
