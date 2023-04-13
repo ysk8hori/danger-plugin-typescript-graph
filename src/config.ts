fdf5ed44f9f0a34e3c749857e48cc707010690b6
ysk8hori <ysk8.hori@gmail.com>
2023-04-13 22:25:37 +0900

    feat(config): tsconfig のルートを指定する環境変数を追加

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,13 @@
+/**
+ * tsconfig を探索するディレクトリ情報を取得する。
+ *
+ * ここで指定した階層から上の階層に向かって tsconfig を探索する。
+ */
+export function getTsconfigRoot() {
+  // 環境変数の TSG_TSCONFIG_ROOT を返却する
+  return process.env.TSG_TSCONFIG_ROOT ?? './';
+}
+
 /** 変更ファイル数が多い場合にグラフの表示を抑止するが、その際のノード数を指定する値を取得する。 */
 export function getMaxSize() {
   // 環境変数の TSG_MAX_SIZE を返却する
