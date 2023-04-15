1ee79658b9330efae948c3f2d75618f845b7a0b3
ysk8hori <ysk8.hori@gmail.com>
2023-04-15 23:21:29 +0900

    docs(log): eslint-disable-next-line @typescript-eslint/no-explicit-any

--- src/log.ts
+++ src/log.ts
@@ -4,6 +4,7 @@ export function log(message?: any, ...optionalParams: any[]): void {
   console.log(stringifyObject(message), ...optionalParams.map(stringifyObject));
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function stringifyObject(message: any) {
   // message がオブジェクトの場合は JSON.stringify で文字列化する
   if (message !== null && typeof message === 'object') {
